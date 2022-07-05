2532b06394a57d77eba50c37b3adc75a5f8ccdad
H-Shira <shirasaka.hinako.17@gmail.com>
2022-07-05 09:26:43 +0900

    task6レビュー後再提出。

--- day6/task6.js
+++ day6/task6.js
@@ -2,11 +2,10 @@
     'use strict';
     kintone.events.on('app.record.create.show', (event) => {
         console.log(event);
-        let addRow = event.record.Table;
-        console.log(addRow);
+        let table = event.record.Table;
 
         //テーブルの配列の初期化
-        addRow.value = [];
+        table.value = [];
 
         //Action5のループ
         const action5 = [
@@ -15,12 +14,12 @@
             '理想への共感',
             '心を動かす',
             '知識を増やす',
-            '公明正大',
+            '公明正大'
         ];
 
         //action5のループが終わるまでAction5にaction5を入れてpushし続ける
-        action5.forEach((title, index) => {
-            addRow.value.push({
+        action5.forEach((title) => {
+            table.value.push({
                 value: {
                     Action5: {type: 'DROP_DOWN', value: title},
                     状況: {type: 'CHECK_BOX', value: ['未振り返り']},
