f7f94e401978e3163533e6197a10fc4626275559
H-Shira <shirasaka.hinako.17@gmail.com>
2022-07-11 17:00:27 +0900

    task9再提出。

--- other tasks/task9.js
+++ other tasks/task9.js
@@ -4,34 +4,26 @@
   let uniqueName;
 
   kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], (event) => {
-    //kintone.events.on(['app.record.create.change.重複禁止項目', 'app.record.edit.change.重複禁止項目'], (event) => {
-      console.log(event);
+    //"uniqueName" = 現在のレコードの重複禁止項目の値
+    uniqueName = event.record.重複禁止項目.value;
+    console.log(uniqueName);
 
-      //"uniqueName" = 現在のレコードの重複禁止項目の値
-      uniqueName = event.record.重複禁止項目.value;
-      console.log(uniqueName);
+    //"uniqueName"と重複する重複禁止項目値を持ったレコードだけを抽出
+    const params = {
+      'fields': '重複禁止項目',
+      'app': 31,
+      'query': '重複禁止項目 = "' + uniqueName + '"'
+    };
 
-      //"uniqueName"と重複する重複禁止項目値を持ったレコードだけを抽出
-      const params = {
-        'fields': '重複禁止項目',
-        'app': 31,
-        'query': '重複禁止項目 = "' + uniqueName + '"'
-      };
+    //「"uniqueName"と重複する重複禁止項目値を持ったレコードが存在していれば」の条件づけ
+    return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params).then((resp) => {
 
-      //「"uniqueName"と重複する重複禁止項目値を持ったレコードが存在していれば」の条件づけ
-      return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params).then((resp) => {
-        console.log(resp);
-        console.log(resp.records);
-
-        if (resp.records.length === 0) {
-          return event;
-        };
-
+      if (resp.records.length !== 0) {
         const response = confirm('レコードが重複しています。このまま保存しますか？');
-        if (!response) {
-          //event.error = 'キャンセルしました';
+        if (response === false) {
           return false;
         }
-      });
+      };
     });
+  });
 })();
