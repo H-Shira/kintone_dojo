7092f25ede7617a161772311d2c2a64c0fe7fff5
H-Shira <shirasaka.hinako.17@gmail.com>
2022-07-11 17:14:57 +0900

    レビュー後再提出。

--- other tasks/task8.js
+++ other tasks/task8.js
@@ -1,43 +1,39 @@
 (() => {
-    'use strict';
-    kintone.events.on('app.record.create.show', (event) => {
-        console.log(event);
-        event.record.Table.value = [];
+  'use strict';
+  kintone.events.on('app.record.create.show', (event) => {
 
-        const params = {
-            app: 38,
-            lang: 'default'
-        };
+    const params = {
+      app: kintone.app.getId(),
+      lang: 'default'
+    };
 
-        return kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', params,) .then ((resp) => {
-            console.log(resp);
+    return kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', params).then((resp) => {
 
-            //action5の選択肢を取り出す
-            //action5の選択肢を配列にする
-            const action5 =resp.properties.Table.fields.Action5.options;
-            console.log(action5);
+      //action5の選択肢を取り出す
+      //action5の選択肢を配列にする
+      const action5 =resp.properties.Table.fields.Action5.options;
+      event.record.Table.value = [];
 
-            //const action5Array = Object.keys(action5);
-            //console.log(action5Array);
+      //const action5Array = Object.keys(action5);
+      //console.log(action5Array);
 
-            const action5Array = [];
-            Object.keys(action5).forEach((key) => {
-                action5Array[action5[key].index] = key
-            });
-            console.log(action5Array);
+      const action5Array = [];
+      Object.keys(action5).forEach((key) => {
+        action5Array[action5[key].index] = key
+      });
 
-            //acton5Arrayをドロップダウンに詰め込んで全種類pushする
-            action5Array.forEach((title) => {
+      //acton5Arrayをドロップダウンに詰め込んで全種類pushする
+      action5Array.forEach((title) => {
 
-                event.record.Table.value.push({
-                    value: {
-                        Action5: {type: 'DROP_DOWN', value: title},
-                        状況: {type: 'CHECK_BOX', value: ['未振り返り']},
-                        課題: {type: 'MULTI_LINE_TEXT', value: ''}
-                    }
-                });
-            });
-            return event;
+        event.record.Table.value.push({
+          value: {
+            Action5: {type: 'DROP_DOWN', value: title},
+            状況: {type: 'CHECK_BOX', value: ['未振り返り']},
+            課題: {type: 'MULTI_LINE_TEXT', value: ''}
+          }
         });
+      });
+      return event;
     });
+  });
 })();
