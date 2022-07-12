d66deec5e8832583454070b767b0e180814469ca
H-Shira <shirasaka.hinako.17@gmail.com>
2022-07-12 11:43:41 +0900

    promise.md追加

--- day5/task5.js
+++ day5/task5.js
@@ -1,2 +1,10 @@
-'use strict';
-
+(() => {
+  'use strict';
+  const news = async () => {
+    const resp = await fetch('https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo'{
+      id: 'dojo'
+    }).then((resp) => {
+      
+    })
+  }
+})();
